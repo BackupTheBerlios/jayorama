e9a6100b5e42da40ebed38f9723ab3b03e0dee51
j_zar <j_zar>
2004-07-28 20:44:22 +0000

    Update 280704

---
+++ OSE/common/common_def.h
@@ -0,0 +1,36 @@
+/* common_def.h
+ *
+ *  Copyright (C) 2004 Gianluca Romanin (aka J_Zar) <jayorama_at_users.sourceforge.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */ 
+
+ 
+
+enum
+{
+	OSE_NULL_FORMAT		=	0,
+	OSE_AUDIO_PCM_16	=	1,		/* Signed 16 bit data */
+	OSE_AUDIO_PCM_24	=	2,		/* Signed 24 bit data */
+	OSE_AUDIO_PCM_32	=	3,		/* Signed 32 bit data */
+	OSE_AUDIO_FLOAT		=	4,		/* 32 bit float data */
+	OSE_AUDIO_DOUBLE	=	5,		/* 64 bit float data */
+	OSE_AUDIO_PCM_INT16	=	6		/* Signed 16 bit integer data */
+} ;
+
+/*** Default label for audio data */
+typedef	int	audioDataType ;
+
+
